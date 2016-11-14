a3255b699104d9b83987eddaead1682fc8b9c0da
Filippo Liverani <fili_live@yahoo.it>
2016-11-14 16:20:53 +0100

    Improved get movies acceptance test readability

--- test/acceptance/get-movies-test.js
+++ test/acceptance/get-movies-test.js
@@ -1,12 +1,13 @@
 const expect = require('chai').expect;
 const supertest = require('supertest-as-promised');
-const endpoint  = process.env.ENDPOINT ? process.env.ENDPOINT : 'http://localhost:3000/movies';
-const api = supertest(endpoint);
+const endpoint  = process.env.ENDPOINT ? process.env.ENDPOINT : 'http://localhost:3000';
+const client = supertest(endpoint);
 
 describe('get movies', function() {
   it('return movie list', function() {
     movies = require('../../movies.json');
-    return api.get('/')
+
+    return client.get('/movies')
       .expect(200)
       .then((res) => {
         expect(res.body).to.eql(movies);
