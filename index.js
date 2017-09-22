7b9dfbf8ce6f9590e4c7459bea7ad1212d954465
Filippo Liverani <filippo-liverani@users.noreply.github.com>
2017-09-22 17:41:59 +0200

    Move response creation to index.js

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const MovieService = require('./lib/movie-service');
 module.exports.movies = (event, context, callback) => {
   const movieService = new MovieService();
   movieService.getMovies()
-    .then((response) => {
+    .then((movies) => {
+      const response = {
+        statusCode: 200,
+        body: movies
+      };
       callback(null, response);
     });
 };
--- test/unit/movie-service-test.js
+++ test/unit/movie-service-test.js
@@ -6,14 +6,12 @@ const MovieService = require('../../lib/movie-service');
 describe('MovieService', function () {
   it('get movies from repository', function () {
     const movies = [{ Title: 'Test Title' }];
-    const response = {
-      statusCode: 200,
-      body: '[{"Title":"Test Title"}]'
-    };
+    const moviesJson = '[{"Title":"Test Title"}]';
+
     const movieRepository = sinon.stub();
     movieRepository.findAll = sinon.stub().returns(Promise.resolve(movies));
     const movieService = new MovieService(movieRepository);
 
-    return expect(movieService.getMovies()).to.eventually.eql(response);
+    return expect(movieService.getMovies()).to.eventually.eql(moviesJson);
   });
 });
